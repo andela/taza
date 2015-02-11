facd470df7867b396436123a6b22c4f6309b7727
andela-tkomolafe <betkomuniq@yahoo.com>
2015-02-11 19:53:34 +0100

    Attempt(2) to fix failing test when deploying

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Taza
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.serve_static_files = true
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
