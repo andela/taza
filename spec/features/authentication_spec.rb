4c9aab0cac99d0ac131f23d2b95a3c3a4a8ee50f
andela-tkomolafe <betkomuniq@yahoo.com>
2015-02-11 19:45:58 +0100

    Attempt to fix failing test when deploying

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "login with google id should redirect to home page", :js => true do
   it "should login with google" do
     visit "/"
-    click_on "Sign in with Google"
+    click_on "Sign In"
 
     fill_in 'Email', :with => 'tolulope.komolafe@andela.co'      # Use your test login id
     fill_in 'Passwd', :with => 'adohim_03'                  # Use your test login password
