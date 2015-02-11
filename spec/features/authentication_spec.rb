4b1fbb599bfe9853da9f4491baa1a0cc9eb8e534
andela-tkomolafe <betkomuniq@yahoo.com>
2015-02-11 18:17:28 +0100

    Attempt to fix testing on circleci

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 describe "login with google id should redirect to home page" do
+  before do
     #puts user_omniauth_authorize_path(:google_oauth2) #/users/auth/google_oauth2
     #Capybara.current_driver = :selenium #change drive according whit https://github.com/jnicklas/capybara#drivers
     #visit  users_omniauth_authorize_path(:google_oauth2)
+  end
   it "should login with google" do
     visit "/"
-    find_link("Sign in with Google").click()
+    click_on "Sign in with Google"
 
     fill_in 'Email', :with => 'calendarizeapp@gmail.com'      # Use your test login id
     fill_in 'Passwd', :with => '1234567'                  # Use your test login password
