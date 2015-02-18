8ea54d8aab6cbfc709308396eb34764479d02946
Enoma Deborah <deborah.enomah@andela.co>
2015-02-18 17:00:17 +0100

    [#88400374] Fixed test for links

--- spec/features/homes_spec.rb
+++ spec/features/homes_spec.rb
@@ -4,9 +4,10 @@ describe "#home page" do
   it "shows all links" do
     visit '/'
 
-    expect(page).to have_link 'About'
-    expect(page).to have_link 'What we do'
-    expect(page).to have_link 'Fellowship'
-    expect(page).to have_content 'Sign In'
+    expect(page).to have_link "Who we are"
+    expect(page).to have_link "Our Fellows"
+    expect(page).to have_link "Your Role"
+    expect(page).to have_link "How it Works"
+    expect(page).to have_content "Sign In"
   end
 end
