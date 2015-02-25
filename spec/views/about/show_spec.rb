b9a1eed01e581ed9caff16602b365162df6fc3de
andela-tkomolafe <tolulope.komolafe@andela.co>
2015-02-25 12:34:19 +0100

    [#88763818 ]Write view spec for about page

---
+++ spec/views/about/show_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "about/show.html.erb" do
+  it 'shows all links' do
+    render
+
+    expect(rendered).to have_link("ABOUT US")
+    expect(rendered).to have_link("MEET THE TEAM")
+  end
+end
