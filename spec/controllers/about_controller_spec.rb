500b1c70777465eade9d40ccb0b24128a4d7876a
Tolulope Komolafe <tolulope.komolafe@andela.co>
2015-03-02 15:43:07 +0100

    [#88763818] added one controller test to about page
    Signed-off-by: Jesmine Omonori <damilola.omonori@andela.co>

--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe AboutController, type: :controller do
+RSpec.describe AboutController do
+  describe "#show" do
+    it "placeholder" do
+      expect(true).to eq(true)
+    end
+  end
 
+  it 'renders the :about view' do
+    get :show
+    expect(response).to render_template :show
+  end
 end
