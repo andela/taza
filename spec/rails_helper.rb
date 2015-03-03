e1bfa09c9b5e53a695b1449795df715bc879160a
Enoma Deborah <deborah.enomah@andela.co>
2015-03-03 13:05:57 +0100

    [#88771170] Introduced factory_girl_rails gem
    Signed-off-by: Ayodeji Jayeoba <ayodeji.jayeoba@andela.co>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
 
-#require 'factory_girl_rails'
+require 'factory_girl_rails'
 
 # require all rb files in the support directory
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
