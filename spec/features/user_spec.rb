be86bb8513d6cb766d70bd8076d7cfcae9c8fa5c
Jesmine Omonori <damilola.omonori@andela.co>
2015-03-09 17:21:32 +0100

    [#88762804] passing feature test for signout
    Signed-off-by: Tolulope Komolafe <tolulope.komolafe@andela.co>

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 feature 'User exists' do
-  scenario 'User visits homepage and signs in' do
+  scenario 'User visits homepage and signs in/signs out' do
     OmniAuth.config.test_mode = true
     set_valid_omniauth
 
     visit '/'
     click_link 'Sign in with Google'
     expect(page).to have_link('Sign Out')
+    click_link 'Sign Out'
+    expect(page).to have_link('Sign in with Google')
   end
 end
