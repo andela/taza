0e29d35fe12795e16a77c09a062a67d3e951d071
Yetunde Sanni <yetunde.sanni@andela.co>
2015-04-09 11:59:57 +0100

    [#87892960] Re-factored test
    Signed-off-by: Enoma Deborah <deborah.enomah@andela.co>

--- spec/controllers/fellows_controller_spec.rb
+++ spec/controllers/fellows_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe FellowsController, type: :controller do
-<<<<<<< HEAD
+
   before do
     user = users(:chad)
     allow(request.env['warden']).to receive(:authenticate!).and_return(user)
@@ -9,9 +9,7 @@ RSpec.describe FellowsController, type: :controller do
     allow(controller).to receive(:current_user).and_return(user)
     get :index
   end
-
-=======
->>>>>>> [#87892960] Added controller and model for a fellow with corresponding test
+  
   it 'renders the fellow profile view' do
     get :index
     expect(response).to render_template :index
